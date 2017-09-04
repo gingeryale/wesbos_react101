382653cf6169c81f7a1d2cb9b75ed90b623b90c0
ao <aolin@ymail.com>
2017-09-04 23:12:04 +0300

    added load samples button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-
+import sampleFishes from '../sample-fishes';
 
 // when this app initializes it needs to know fish state
 // Create a constructor method and call super, before using this
@@ -11,6 +11,8 @@ class App extends React.Component {
 		super();
 		// bind addFish method to constructor for this
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+
 		// getInitialState
 		this.state = {
 			fishes: {},
@@ -27,6 +29,14 @@ class App extends React.Component {
 		// set state specific piece of state
 		   this.setState( { fishes} ); // fishes: fishes in es6 don't need both fishes
 	}
+
+	// State lives in our App
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -34,7 +44,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood market"/>
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,7 +7,9 @@ class Inventory extends React.Component {
 		return (
 			<div className="inventory">
 				<h2>Inventory</h2>
-				<AddFishForm addFish={this.props.addFish} /> // pass data from func thru props, access func inside form
+				<AddFishForm addFish={this.props.addFish} /> 
+				{/* pass data from func thru props, access func inside form */}
+				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
 		)
 	}
